c13bd1b030b10f84a202a9faa96432e73a8fb8af
Rében Almeida <ruben.almeida@innovpoint.com>
2025-05-03 21:48:10 +0100

    Revert FabChat

--- src/components/fab/FabChat.jsx
+++ src/components/fab/FabChat.jsx
@@ -5,7 +5,7 @@ import VoiceChatIcon from '@mui/icons-material/VoiceChat';
 const FabChat = (props) => {
     return (
         <Fab id="fab-chat" color="primary" variant="extended" onClick={() => { props.setChatBot(!props.chatBot) }} >
-            <VoiceChatIcon sx={{ mr: 1 }} />Falamos?
+            <VoiceChatIcon sx={{ mr: 1 }} />Posso Ajudar?
         </Fab>
     )
 }
