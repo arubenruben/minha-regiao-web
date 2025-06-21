28c765f6c1546e8d9f105d18aa219e3e3d890532
Rében Almeida <ruben.almeida@innovpoint.com>
2025-06-21 20:18:23 +0100

    solved major issue that inertia routes were not being updated

--- docker/production/php-fpm/entrypoint.sh
+++ docker/production/php-fpm/entrypoint.sh
@@ -25,8 +25,15 @@ php artisan migrate --force
 # -----------------------------------------------------------
 # Improves performance by caching config and routes.
 # -----------------------------------------------------------
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+php artisan cache:clear
+
 php artisan config:cache
 php artisan route:cache
+php artisan view:cache
+php artisan optimize
 
 # Run the default command
 exec "$@"
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel(['resources/js/app.jsx']),
-        react(),
+        react(),    
     ],
     server: {
         host: '0.0.0.0',
