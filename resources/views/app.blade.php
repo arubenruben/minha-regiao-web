f2cefd4cae6c125710cc989267980c04a65c6620
Rében Almeida <ruben.almeida@innovpoint.com>
2025-07-04 13:10:04 +0100

    Update .env and blade to introduce a web description indexable by Search Engines

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta name="description"
+        content="Os Distritos, Municípios e Freguesias de Portugal. Eleições, notícias e informações sobre a tua região.">
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
